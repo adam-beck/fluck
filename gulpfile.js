57b9738116fc4462bee23be53abb7770e8bb3a10
Adam Beck <AdamB.Infinity@gmail.com>
2016-02-16 21:49:37 -0500

    Use postcss

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ var browserSync = require('browser-sync').create();
 var port = process.env.PORT || 8080;
 var sass = require('gulp-sass');
 
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var postcssImport = require('postcss-import');
+var autoprefixer = require('autoprefixer');
+var cssNext = require('postcss-cssnext');
+
 gulp.task('serve', ['watch'], function() {
   browserSync.init({
     proxy: 'http://localhost:' + port,
@@ -12,6 +18,22 @@ gulp.task('serve', ['watch'], function() {
 
 });
 
+gulp.task('styles', function() {
+
+  var processors = [
+    postcssImport,
+    autoprefixer,
+    cssNext
+  ];
+
+  return gulp.src('./css/index.css')
+  .pipe(sourcemaps.init())
+  .pipe(postcss(processors))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./assets/css'))
+  .pipe(browserSync.stream());
+});
+
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
   .pipe(sass().on('error', sass.logError))
@@ -19,8 +41,13 @@ gulp.task('sass', function() {
   .pipe(browserSync.stream());
 });
 
-gulp.task('watch', ['sass'], function() {
-  gulp.watch('sass/**/*.scss', ['sass']);
+// gulp.task('watch', ['sass'], function() {
+//   gulp.watch('sass/**/*.scss', ['sass']);
+//   gulp.watch('./**/*.hbs').on('change', browserSync.reload);
+// });
+
+gulp.task('watch', ['styles'], function() {
+  gulp.watch('css/**/*.css', ['styles']);
   gulp.watch('./**/*.hbs').on('change', browserSync.reload);
 });
 
